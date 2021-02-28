caafcdc7206deb9df0ebbde1f284795308fa0e00
Figo Aranta <figoaranta@Figos-MacBook-Pro-3.local>
2021-02-28 20:46:37 +0700

    fix mobile version

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 
 app.get('/', async (req, res) => {
+    // res.send("Hello world")
     const QR = await scraper.scrapeQR();
     res.send(`<img src='${QR}' alt="Figo's QR" width="350" height="350">`);
 });
